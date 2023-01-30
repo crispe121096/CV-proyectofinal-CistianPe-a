04fef9cdb841f2b433b21502275d24cb673c3cd1
Cristian Peña <cristianfrancisco41@gmail.com>
2023-01-30 07:41:08 -0300

    agregue comentarios y modifique la foto de perfil y logo

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,3 +1,7 @@
+
+/* Un pequeño archivo java scrip que me permiteabrir y cerrar el menu mientras no sea dispositivo de escritorio  */
+
+
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
